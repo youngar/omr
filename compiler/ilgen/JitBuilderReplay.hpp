8463c59e79f20f6a158e72e57d8efd991c9821d5
Sharon Wang <sharon-wang-cpsc@outlook.com>
2018-05-28 10:14:02 -0400

    Added files to be implemented for replay functionality

---
+++ compiler/ilgen/JitBuilderReplay.hpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *
+ * (c) Copyright IBM Corp. 2018, 2018
+ *
+ *  This program and the accompanying materials are made available
+ *  under the terms of the Eclipse Public License v1.0 and
+ *  Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Contributors:
+ *    Multiple authors (IBM Corp.) - initial implementation and documentation
+ ******************************************************************************/
+
+namespace OMR
+{
+   class JitBuilderReplay
+      {
+
+      };
+
+} // namespace OMR
+
+namespace TR
+{
+   class JitBuilderReplay : public OMR::JitBuilderReplay
+      {
+
+      };
+
+} // namespace TR
